31b8a29b0959bc2ebabd69536c94306817802673
Beryl01 <berylnegesa@gmail.com>
2020-06-01 00:56:47 +0300

    Working on cloudinary

--- static/js/main.js
+++ static/js/main.js
@@ -1,14 +1,3 @@
-var myWidget = cloudinary.createUploadWidget({
-  cloudName: 'my_cloud_name', 
-  uploadPreset: 'my_preset'}, (error, result) => { 
-    if (!error && result && result.event === "success") { 
-      console.log('Done! Here is the image info: ', result.info); 
-    }
-  }
-)
-document.getElementById("upload_widget").addEventListener("click", function(){
-    myWidget.open();
-  }, false);
 console.log("readyee")
 $(document).ready(function() {
   $('.row .like_u').click(function() {
